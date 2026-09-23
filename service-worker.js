7023dc91cdcee81200a6b646344631fbb7bb03cf
rodadeprojetos1-afk <rodadeprojetos1-afk@users.noreply.github.com>
2026-09-23 09:26:55 -0300

    Atualiza PWA antes de usar cache

--- service-worker.js
+++ service-worker.js
@@ -11,11 +11,11 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+  event.respondWith(fetch(event.request).then(response => {
     const copy = response.clone();
     caches.open(CACHE).then(cache => cache.put(event.request, copy));
     return response;
-  }).catch(() => caches.match('./index.html'))));
+  }).catch(() => caches.match(event.request).then(cached => cached || caches.match('./index.html'))));
 });
 
 self.addEventListener('message', event => {
